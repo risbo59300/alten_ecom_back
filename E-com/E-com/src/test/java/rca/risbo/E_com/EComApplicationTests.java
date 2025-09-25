2159851b3539bd297d63bb85d247318c57abfa2b
Boris <ybgabamano@gmail.com>
2025-09-25 10:16:52 +0200

    Init project

---
+++ E-com/E-com/src/test/java/rca/risbo/E_com/EComApplicationTests.java
@@ -0,0 +1,13 @@
+package rca.risbo.E_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
